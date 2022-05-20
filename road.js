dd1e454c66029d947ac06c8d0cf4db73a9b00400
Raju Roy <rajuroy258500@gmail.com>
2022-05-20 11:18:36 +0530

    Car is static and lane moves but right border missing

--- road.js
+++ road.js
@@ -15,8 +15,7 @@ class Road{
         const topLeft={x:this.left,y:this.top};
         const topRight={x:this.Right,y:this.top};
         const bottomLeft={x:this.left,y:this.bottom};
-        const bottomRight={x:this.right,y:this.bottom};
-       
+        const bottomRight={x:this.right,y:this.bottom};       
 
         this.borders=[
             [topLeft,bottomLeft],
@@ -42,18 +41,21 @@ class Road{
                 this.right,
                 i/this.laneCount
             );
-            if(i>0 && i<this.laneCount)
-            {
-                ctx.setLineDash([20,20]);
-            }
-            else 
-                ctx.setLineDash([]);
+            
+            ctx.setLineDash([20,20]);            
 
             ctx.beginPath();
             ctx.moveTo(x,this.top);
             ctx.lineTo(x,this.bottom);
             ctx.stroke();
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+        });
     }
 }
 
