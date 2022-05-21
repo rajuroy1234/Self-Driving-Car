73df703584096e9f1df062adf42b42359c51bf72
Raju Roy <rajuroy258500@gmail.com>
2022-05-21 14:09:04 +0530

    Added Sensors. Needs Refactoring at borders.

--- car.js
+++ car.js
@@ -11,12 +11,15 @@ class Car {
         this.friction=0.05;
         this.angle=0;
 
+        this.sensor=new Sensor(this);
+
         this.controls = new Controls();
     }
 
     update()
     {
         this.#move();
+        this.sensor.update();
     }
 
     #move()
@@ -70,5 +73,7 @@ class Car {
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
